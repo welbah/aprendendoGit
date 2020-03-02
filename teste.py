82833ffa8ad0df76f9f6d19b6961e625599da83d
Welder Silva <welderpaulo.93@gmail.com>
2020-03-02 14:26:52 -0300

    Teste commit

---
+++ teste.py
@@ -0,0 +1,12 @@
+def main():
+    a = int(input("Digite um numero"))
+    b = int(input("Digite outro numero"))
+    soma = a + b
+
+    print("A soma de a + b = " ,soma)
+
+main()
+
+print("A alteração do codigo")
+print("A alteração do codigo 2")
+print("A alteração do codigo 2")
